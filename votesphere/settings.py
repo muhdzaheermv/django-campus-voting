6b46fe70f479a0ab789e9f104e3e1ac8b8c1353e
muhdzaheermv <muhdzaheermv@gmail.com>
2025-06-27 05:25:50 +0530

    Add STATIC_ROOT and WhiteNoise for static file handling

--- votesphere/settings.py
+++ votesphere/settings.py
@@ -139,6 +139,7 @@ LOGOUT_REDIRECT_URL = '/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "vote/static"]
 
 
