7c5e8e8582eb2a2f27b9db5f49e71d251c3557bc
muhdzaheermv <muhdzaheermv@gmail.com>
2025-06-27 05:58:43 +0530

    allowed hosts updated

--- votesphere/settings.py
+++ votesphere/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 DEBUG = True
