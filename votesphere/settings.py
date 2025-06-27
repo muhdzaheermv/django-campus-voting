9d26f1c2175594fc1f7b133785c18642a614ff16
muhdzaheermv <muhdzaheermv@gmail.com>
2025-06-27 05:35:00 +0530

    Fix middleware order: sessions before auth

--- votesphere/settings.py
+++ votesphere/settings.py
@@ -64,13 +64,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware' #Uncomment out the lines to run the project locally
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware' #Uncomment out the lines to run the project locally
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
